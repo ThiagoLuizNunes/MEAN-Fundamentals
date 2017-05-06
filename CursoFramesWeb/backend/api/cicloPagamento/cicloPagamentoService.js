79a651bdb1b0348565b74dbc819e9430a1ddced8
ThiagoLuizNunes <thiago.luiz@lavid.ufpb.br>
2017-05-06 16:58:44 -0300

    error handling post and puth methods

--- CursoFramesWeb/backend/api/cicloPagamento/cicloPagamentoService.js
+++ CursoFramesWeb/backend/api/cicloPagamento/cicloPagamentoService.js
@@ -1,7 +1,28 @@
 const CicloPagamento = require('./cicloPagamento')
+const _ = require('lodash')
 
 CicloPagamento.methods(['get', 'post', 'put', 'delete'])
 CicloPagamento.updateOptions({new: true, runValidators: true})
+
+//Tratamento de erros após métodos POST e PUT
+CicloPagamento.after('post', sendErrosOrNext).after('put', sendErrosOrNext)
+
+function sendErrosOrNext(req, res, next) {
+    const bundle = res.locals.bundle
+
+    if(bundle.errors) {
+      var errors = parseErrors(bundle.errors)
+      res.status(500).json({errors})
+    } else {
+      next()
+    }
+}
+
+function parseErrors(nodeRestfulErrors) {
+  const errors = []
+  _.forIn(nodeRestfulErrors, error => errors.push(error.message))
+  return errors
+}
 
 //Retornar quantidade de registros na collection
 CicloPagamento.route('count', function(req, res, next){
