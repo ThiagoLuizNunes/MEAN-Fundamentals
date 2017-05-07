295a4baf5fdf40cfdcc7ea06d74f32cb99c7baf6
ThiagoLuizNunes <thiago.luiz@lavid.ufpb.br>
2017-05-07 01:51:50 -0300

    making dinex.html and index.js and some .css

--- CursoFramesWeb/angular1/gulpTasks/deps.js
+++ CursoFramesWeb/angular1/gulpTasks/deps.js
@@ -1,41 +1,40 @@
 const gulp = require('gulp')
 const uglify = require('gulp-uglify')
-const concat = require('gulp-concat')
 const uglifycss = require('gulp-uglifycss')
+const concat = require('gulp-concat')
 
 gulp.task('deps', ['deps.js', 'deps.css', 'deps.fonts'])
 
-gulp.task('deps.js', function(){
+gulp.task('deps.js', function() {
   gulp.src([
     'node_modules/angular/angular.min.js',
-    'node_modules/angular-iu-router/release/angular-ui-router.min.js',
+    'node_modules/angular-ui-router/release/angular-ui-router.min.js',
     'node_modules/angular-animate/angular-animate.min.js',
-    'node_modules/angular-toast/dist/angular-toast.min.js',
+    'node_modules/angular-toastr/dist/angular-toastr.tpls.min.js',
     'node_modules/admin-lte/plugins/jQuery/jquery-2.2.3.min.js',
     'node_modules/admin-lte/bootstrap/js/bootstrap.min.js',
     'node_modules/admin-lte/plugins/slimScroll/jquery.slimscroll.min.js',
-    'node_modules/admin-lte/dist/js/app.min.js'
+    'node_modules/admin-lte/dist/js/app.min.js',
   ])
-  .pipe(uglify()) //Diminuir o arquivo .js menor possivel
-  .pipe(concat('deps.min.js'))  //Concatenou todos os aruivos .js em um unico
+  .pipe(uglify()) //Diminuir o arquivo .js, torna-lo menor possivel
+  .pipe(concat('deps.min.js')) //Concatenou todos os arquivos .js em um unico
   .pipe(gulp.dest('public/assets/js')) //Salva o arquivo minificado na pasta desejada
 })
 
-gulp.task('deps.css', function(){
+gulp.task('deps.css', function() {
   gulp.src([
-    'node_modules/angular-toast/dist/angular-toast.min.js',
+    'node_modules/angular-toastr/dist/angular-toastr.min.css',
     'node_modules/font-awesome/css/font-awesome.min.css',
-    'node_modules/admin-lte/bootstrap/css/bootstrap.min.js',
+    'node_modules/admin-lte/bootstrap/css/bootstrap.min.css',
     'node_modules/admin-lte/dist/css/AdminLTE.min.css',
-    'node_modules/admin-lte/dist/skins/_all-skins.min.css'
-
+    'node_modules/admin-lte/dist/css/skins/_all-skins.min.css',
   ])
   .pipe(uglifycss({ "uglyComments": true }))
   .pipe(concat('deps.min.css'))
   .pipe(gulp.dest('public/assets/css'))
 })
 
-gulp.task('deps.fonts', function(){
+gulp.task('deps.fonts', function() {
   gulp.src([
     'node_modules/font-awesome/fonts/*.*',
     'node_modules/admin-lte/bootstrap/fonts/*.*'
--- CursoFramesWeb/angular1/gulpTasks/server.js
+++ CursoFramesWeb/angular1/gulpTasks/server.js
@@ -2,7 +2,7 @@ const gulp = require('gulp')
 const watch = require('gulp-watch')
 const webserver = require('gulp-webserver')
 
-gulp.task('server', ['watch'], function(){
+gulp.task('server', ['watch'], function() {
   gulp.src('public').pipe(webserver({
     livereload: true,
     port: 3000,
@@ -10,11 +10,9 @@ gulp.task('server', ['watch'], function(){
   }))
 })
 
-//Task que monitora os arquivos, uma vez modificado sera automaticamento feito o BUILD
-//e o browser sera restartado
-gulp.task('watch', function(){
+gulp.task('watch', function() {
   watch('app/**/*.html', () => gulp.start('app.html'))
   watch('app/**/*.css', () => gulp.start('app.css'))
   watch('app/**/*.js', () => gulp.start('app.js'))
-  watch('app/**/*.*', () => gulp.start('app.assets'))
+  watch('assets/**/*.*', () => gulp.start('app.assets'))
 })
