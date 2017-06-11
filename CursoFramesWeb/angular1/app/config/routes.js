d77d6a01cd05406c865dad8c8672e4d9ed69401b
ThiagoLuizNunes <thiago.luiz@lavid.ufpb.br>
2017-06-11 03:10:27 -0300

    adding components

---
+++ CursoFramesWeb/angular1/app/config/routes.js
@@ -0,0 +1,16 @@
+angular.module('primeiraApp').config([
+  //Injeção de dependência
+  '$stateProvider',
+  '$urlRouterProvider',
+  function ($stateProvider, $urlRouterProvider) {
+    $stateProvider.state('dashboard', {
+      url: "/dashboard",
+      templateUrl: "dashboard/dashboard.html"
+    }).state('cicloPagamentos', {
+      url: "/cicloPagamentos",
+      templateUrl: "cicloPagamentos/tabs.html"
+    })
+
+    $urlRouterProvider.otherwise('/dashboard')
+  }
+])
