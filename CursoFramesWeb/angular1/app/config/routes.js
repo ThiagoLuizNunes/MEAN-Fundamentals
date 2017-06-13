39c7150cfc5684cebb69ac28f78b6675e6151e7d
ThiagoLuizNunes <thiago.luiz@lavid.ufpb.br>
2017-06-13 02:44:28 -0300

    adding field component and form,list and tabs html

--- CursoFramesWeb/angular1/app/config/routes.js
+++ CursoFramesWeb/angular1/app/config/routes.js
@@ -1,16 +1,18 @@
-angular.module('primeiraApp').config([
-  //Injeção de dependência
-  '$stateProvider',
-  '$urlRouterProvider',
-  function ($stateProvider, $urlRouterProvider) {
-    $stateProvider.state('dashboard', {
-      url: "/dashboard",
-      templateUrl: "dashboard/dashboard.html"
-    }).state('cicloPagamentos', {
-      url: "/cicloPagamentos",
-      templateUrl: "cicloPagamentos/tabs.html"
-    })
+(function () {
+  angular.module('primeiraApp').config([
+    //Injeção de dependência
+    '$stateProvider',
+    '$urlRouterProvider',
+    function ($stateProvider, $urlRouterProvider) {
+      $stateProvider.state('dashboard', {
+        url: "/dashboard",
+        templateUrl: "dashboard/dashboard.html"
+      }).state('cicloPagamentos', {
+        url: "/cicloPagamentos",
+        templateUrl: "cicloPagamentos/tabs.html"
+      })
 
-    $urlRouterProvider.otherwise('/dashboard')
-  }
-])
+      $urlRouterProvider.otherwise('/dashboard')
+    }
+  ])
+})()
