7b409d5ed4f05ed127f0cfba25b6c156c56117af
ThiagoLuizNunes <thiago.luiz@lavid.ufpb.br>
2017-06-13 21:08:15 -0300

    error 500 by sending resolved

--- CursoFramesWeb/angular1/app/cicloPagamentos/cicloPagamentosController.js
+++ CursoFramesWeb/angular1/app/cicloPagamentos/cicloPagamentosController.js
@@ -6,7 +6,11 @@
 
   function CicloPagamentoController($http){
     const vm = this
-
+    vm.cicloPagamento = {
+      nome: 'Nome',
+      mes: 1,
+      ano: 2017
+    }
     vm.create = function(){
       const url = 'http://localhost:3003/api/cicloPagamento/'
       $http.post(url, vm.cicloPagamento).then(function (response) {
