b68a833438b3c75527525847223481981a87109e
ThiagoLuizNunes <thiago.luiz@lavid.ufpb.br>
2017-06-13 21:58:51 -0300

    adding new factory msgsFactory to treat errors at API

--- CursoFramesWeb/angular1/app/cicloPagamentos/cicloPagamentosController.js
+++ CursoFramesWeb/angular1/app/cicloPagamentos/cicloPagamentosController.js
@@ -1,21 +1,25 @@
 (function () {
   angular.module('primeiraApp').controller('CicloPagamentoCtrl', [
     '$http',
+    'msgs',
     CicloPagamentoController
   ])
 
-  function CicloPagamentoController($http){
+  function CicloPagamentoController($http, msgs){
     const vm = this
-    vm.cicloPagamento = {
-      nome: 'Nome',
-      mes: 1,
-      ano: 2017
-    }
+    // vm.cicloPagamento = {
+    //   nome: 'Nome',
+    //   mes: 1,
+    //   ano: 2017
+    // }
     vm.create = function(){
       const url = 'http://localhost:3003/api/cicloPagamento/'
       $http.post(url, vm.cicloPagamento).then(function (response) {
         vm.cicloPagamento = {}
-        console.log('Sucesso!');
+        msgs.addSuccess('Operação realizada com sucesso!!')
+      }).catch(function (response) {
+        //Errors foi definido no backend
+        msgs.addError(response.data.errors)
       })
     }
   }
