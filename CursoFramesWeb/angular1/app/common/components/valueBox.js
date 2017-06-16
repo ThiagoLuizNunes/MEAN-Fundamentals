40f8f98fddb95849412afcd2e585dedfdbd3e68d
ThiagoLuizNunes <thiago.luiz@lavid.ufpb.br>
2017-06-16 18:38:25 -0300

    gridSystem factory problema was fixed up

--- CursoFramesWeb/angular1/app/common/components/valueBox.js
+++ CursoFramesWeb/angular1/app/common/components/valueBox.js
@@ -9,22 +9,22 @@
     },
     controller: [
       'gridSystem',
-      function (gridSystem) {
+      function(gridSystem) {
         this.$onInit = () => this.gridClasses = gridSystem.toCssClasses(this.grid)
       }
     ],
     template: `
     <div class="{{ $ctrl.gridClasses }}">
-        <div class="small-box {{ $ctrl.colorClass }}">
+      <div class="small-box {{ $ctrl.colorClass }}">
         <div class="inner">
           <h3>{{ $ctrl.value }}</h3>
           <p>{{ $ctrl.text }}</p>
         </div>
         <div class="icon">
-          <i class="{{ $ctrl.iconClass}}"></i>
+          <i class="{{ $ctrl.iconClass }}"></i>
         </div>
       </div>
     </div>
     `
-  })
+  });
 })()
